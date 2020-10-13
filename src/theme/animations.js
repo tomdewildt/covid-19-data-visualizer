3d28db9850402de0720c3b6abba42ad32b513926
tomdewildt <t.l.dewildt@gmail.com>
2020-10-13 17:19:46 +0200

    Update theme

--- src/theme/animations.js
+++ src/theme/animations.js
@@ -1,3 +1,23 @@
 import { css } from "styled-components";
 
-export default css``;
+export default css`
+    @keyframes ripple {
+        from {
+            opacity: 1;
+            transform: scale3d(0.75,0.75,1);
+        }
+        to {
+            opacity: 0;
+            transform: scale3d(1.5,1.5,1);
+        }
+    }
+
+    @keyframes fade {
+        from {
+            opacity: 1;
+        }
+        to {
+            opacity: 0;
+        }
+    }
+`;
